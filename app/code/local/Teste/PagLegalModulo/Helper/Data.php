19d6cdc64ec34326fa51eaf8bb93c3e290c7c1ce
amandacarolinacardoso <amandacarolinacardoso02@gmail.com>
2017-12-22 02:52:57 -0200

    Tirando comentario

--- app/code/local/Teste/PagLegalModulo/Helper/Data.php
+++ app/code/local/Teste/PagLegalModulo/Helper/Data.php
@@ -1,8 +1,2 @@
 <?php
-class Teste_PagLegalModulo_Helper_Data extends Mage_Payment_Helper_Data
-{
-    /*function getPaymentGatewayUrl()
-    {
-        return Mage::getUrl('custompaymentmethod/payment/gateway', array('_secure' => false));
-    }*/
-}
+class Teste_PagLegalModulo_Helper_Data extends Mage_Payment_Helper_Data {}
